772a377797ba69ff21a22fe69a0e960c0458233f
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-04-14 12:55:56 +0300

    Update Detect Pangram 6 kyu.py

--- Detect Pangram 6 kyu.py
+++ Detect Pangram 6 kyu.py
@@ -9,3 +9,12 @@
 def is_pangram(st):
     alphabet ="abcdefghijklmnopqrstuvwxyz"
     return set(alphabet) <= set(st.lower())
+
+# import string
+
+# def is_pangram(s):
+#     s = s.lower()
+#     for char in 'abcdefghijklmnopqrstuvwxyz':
+#         if char not in s:
+#             return False
+#     return True
