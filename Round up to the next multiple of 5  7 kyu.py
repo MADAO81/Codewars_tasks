7b6811887c01e29d657456f2952ea964515d27fb
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-04-24 20:39:37 +0300

    Create Round up to the next multiple of 5  7 kyu.py

---
+++ Round up to the next multiple of 5  7 kyu.py
@@ -0,0 +1,7 @@
+# Round up to the next multiple of 5
+# Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+
+import math
+def round_to_next5(n):
+    return math.ceil(n/5)*5
