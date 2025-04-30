4fe23e9ba7cbfc2918c070403a0d700de610c6dc
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-04-30 20:08:22 +0300

    Update Highest and Lowest 7 kyu.py

--- Highest and Lowest 7 kyu.py
+++ Highest and Lowest 7 kyu.py
@@ -16,3 +16,13 @@ def high_and_low(numbers):
     numbers = [int(i) for i in numbers.split()]
 
     return " ".join([str(max(numbers)), str(min(numbers))])
+
+
+# def high_and_low(numbers):
+#     result_line = []
+#     result = ""
+#     for i in numbers.split(" "):
+#         result_line.append(int(i))
+#     result_line = sorted(result_line)
+#     result = str(result_line[-1]) + " " + str(result_line[0]) 
+#     return result
