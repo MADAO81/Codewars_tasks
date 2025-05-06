e7c9c5972892722212184f8798c9990878990b7a
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-05-06 14:26:18 +0300

    Update How good are you really 8 kyu.py

--- How good are you really 8 kyu.py
+++ How good are you really 8 kyu.py
@@ -17,3 +17,5 @@ def better_than_average(class_points, your_points):
 #     average = (sum(class_points) + your_points) / (len(class_points) + 1)
 #     return your_points > average
 
+# def better_than_average(class_points, your_points)
+# return your_points > sum(class_points) / len(class_points)
