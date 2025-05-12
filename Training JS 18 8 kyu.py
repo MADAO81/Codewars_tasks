9439a649a8162644c8913d61baed399d78d8dc68
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-05-12 12:37:42 +0300

    Create Training JS 18 8 kyu.py

---
+++ Training JS 18 8 kyu.py
@@ -0,0 +1,4 @@
+# Training JS #18: Methods of String object--concat() split() and its good friend join()
+
+def split_and_merge(string, sp):
+    return ' '.join(sp.join(word) for word in string.split())
