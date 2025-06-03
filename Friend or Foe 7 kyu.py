452c47ff6dfe785f1b568abf77602fc3dd354dfa
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-06-03 15:10:30 +0300

    Create Friend or Foe 7 kyu.py

---
+++ Friend or Foe 7 kyu.py
@@ -0,0 +1,24 @@
+# Friend or Foe?
+# Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+# If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+# Input = ["Ryan", "Kieran", "Jason", "Yous"]
+# Output = ["Ryan", "Yous"]
+
+# Input = ["Peter", "Stephen", "Joe"]
+# Output = []
+# Input strings will only contain letters.
+# Note: keep the original order of the names in the output.
+
+
+def friend(x):
+    friend_list = []
+    for person in x:
+        if len(person) == 4:
+            friend_list.append(person)
+    return friend_list        
+    
+    
+# def friend(x):
+#     return [f for f in x if len(f) == 4]
