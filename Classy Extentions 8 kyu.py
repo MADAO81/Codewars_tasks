93621cb7d4a41a704c7dc0670b3507001075776a
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-06-07 06:21:34 +0300

    Create Classy Extentions 8 kyu.py

---
+++ Classy Extentions 8 kyu.py
@@ -0,0 +1,16 @@
+# Classy Extentions
+# The goal of this kata is to train the basic OOP concepts of inheritance and method overriding.
+
+# You will be preloaded with the Animal class, so you should only edit the Cat class.
+
+# Task
+# Your task is to complete the Cat class which extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
+
+# The name attribute is accessible in the class with self.name.
+
+from preloaded import Animal
+
+class Cat(Animal):
+    def speak(self):
+        return f"{self.name} meows."
+    
