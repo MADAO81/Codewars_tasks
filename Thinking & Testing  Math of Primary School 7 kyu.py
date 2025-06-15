00405355c4d59390247af88de9f20d880d75c366
Black Dow <Atyz@7324643>
2025-06-15 11:50:06 +0300

    june_sunday

---
+++ Thinking & Testing  Math of Primary School 7 kyu.py
@@ -0,0 +1,9 @@
+# Thinking & Testing : Math of Primary School
+# No Story
+# No Description
+# Only by Thinking and Testing
+# Look at the results of the testcases, and guess the code!
+
+
+def mystery(a):
+    return a[0]*a[3] + a[1]*a[2]
