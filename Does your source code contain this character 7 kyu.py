5794e96e6169133004341ab9d55686272207636e
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-06-17 11:57:39 +0300

    Create Does your source code contain this character 7 kyu.py

---
+++ Does your source code contain this character 7 kyu.py
@@ -0,0 +1,11 @@
+# [Code Golf] Does your source code contain this character?
+# Given a character, return True if your source code (the one from solution tab) contains this character or False if it doesn't.
+
+# Example
+# d('d')  # probably True, because the name of the function is 'd', unless you obfuscate it
+# Importing is disabled. You also may not use open('solution.py') to fetch your code from the file system, that's cheating.
+
+# Keep your code short (under 33 characters)
+
+
+d=lambda a:a in' :=lambdain\'\\'
