43b41bfa943c0c177ace30ea48f87b055a501eb3
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-06-24 09:48:15 +0300

    Create Batman Quotes 7 kyu.py

---
+++ 7 kyu/Batman Quotes 7 kyu.py
@@ -0,0 +1,22 @@
+# Batman & Robin have gotten into quite a pickle this time. The Joker has mixed up their iconic quotes and also replaced one of the characters in their names, with a number. They need help getting things back in order.
+
+# Implement the getQuote method which takes in an array of quotes, and a string comprised of letters and a single number (e.g. "Rob1n") where the number corresponds to their quote indexed in the passed in array.
+
+# Return the quote along with the character who said it:
+
+# BatmanQuotes.getQuote(["I am vengeance. I am the night. I am Batman!", "Holy haberdashery, Batman!", "Let's put a smile on that faaaceee!"], "Rob1n") should output =>  "Robin: Holy haberdashery, Batman!
+# Hint: You are guaranteed that the number in the passed in string is placed somewhere after the first character of the string. The quotes either belong to "Batman", "Robin", or "Joker".
+
+class BatmanQuotes(object):
+    l = ["Batman", "Robin", "Joker"]
+    @staticmethod
+    def get_quote(quotes, hero):
+        i = next((int(x) for x in hero if x.isdigit()))
+        return BatmanQuotes.l[i] + ": " + quotes[i]
+
+
+# import re
+# class BatmanQuotes(object):
+#   @staticmethod
+#   def get_quote(quotes, hero):
+#     return {'B':'Batman', 'R':'Robin', 'J':'Joker'}[hero[0]] + ": " + quotes[int(re.search('\d+', hero).group())]
