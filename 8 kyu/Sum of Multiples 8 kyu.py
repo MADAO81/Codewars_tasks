bf76c58f0c0e8ee10f4aff0b01f7e0bce623d403
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-07-09 09:54:57 +0300

    Create Sum of Multiples 8 kyu.py

---
+++ 8 kyu/Sum of Multiples 8 kyu.py
@@ -0,0 +1,26 @@
+# Your Job
+# Find the sum of all multiples of n below m
+
+# Keep in Mind
+# n and m are natural numbers (positive integers)
+# m is excluded from the multiples
+# Examples
+# sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+# sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+# sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+# sumMul(4, -7)  ==> "INVALID"
+
+def sum_mul(n, m):
+    if n <= 0 or m <= 0:
+        return 'INVALID'
+    result = 0
+    for i in range(m):
+        if i%n == 0:
+            result +=i
+    return result
+
+# def sum_mul(n, m):
+#     if m>0 and n>0:
+#         return sum(range(n, m, n))
+#     else:
+#         return 'INVALID'
