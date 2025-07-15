4f2b08af916d9682bf9414fd43124670d1d64712
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-07-15 10:59:51 +0300

    Create Find the Mine 6 kyu.py

---
+++ 6 kyu/Find the Mine 6 kyu.py
@@ -0,0 +1,29 @@
+# You've just discovered a square (NxN) field and you notice a warning sign. 
+# The sign states that there's a single bomb in the 2D grid-like field in front of you.
+
+# Write a function that accepts a 2D array, and returns the location of the mine. 
+# The mine is represented as the integer 1 in the 2D array. Areas in the 2D array that are not the mine will be represented as 0s.
+
+# The location returned should be an array (Tuple<int, int> in C#, RAX:RDX in NASM) where the first element 
+# is the row index, and the second element is the column index of the bomb location (both should be 0 based). 
+# All 2D arrays passed into your function will be a square (NxN), and there will only be one mine in the array.
+
+# Examples (Input --> Output)
+# [ [1, 0, 0], [0, 0, 0], [0, 0, 0] ] --> [0, 0]
+
+# [ [0, 0, 0], [0, 1, 0], [0, 0, 0] ] --> [1, 1]
+
+# [ [0, 0, 0], [0, 0, 0], [0, 1, 0] ] --> [2, 1]
+
+def mine_location(field):
+    for row_index, row in enumerate(field):
+        for colon_index, cell in enumerate(row):
+            if cell == 1:
+                return [row_index,colon_index]
+
+
+
+
+# def mineLocation(field):
+#     for subfield in field:
+#         if 1 in subfield: return [field.index(subfield), subfield.index(1)]
