646bb2c930fbca565206b8ca2bc2ffbda4459c3c
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-07-16 17:36:33 +0300

    Create The reject function 7 kyu.py

---
+++ 7 kyu/The reject function 7 kyu.py
@@ -0,0 +1,6 @@
+# Implement a function which filters out array values which satisfy the given predicate.
+
+# reject([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)  =>  [1, 3, 5]
+
+def reject(seq, predicate): 
+    return [item for item in seq if not predicate(item)]
