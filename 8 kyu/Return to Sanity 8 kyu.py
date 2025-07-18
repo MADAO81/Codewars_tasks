d1fd49490359b4a09640bddcde65b3a2f8388d24
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-07-18 15:56:49 +0300

    Create Return to Sanity 8 kyu.py

---
+++ 8 kyu/Return to Sanity 8 kyu.py
@@ -0,0 +1,9 @@
+# This function should return an object, but it's not doing what's intended. What's wrong?
+
+def mystery():
+    results = {
+    'sanity': 'Hello'}
+    return results
+
+# def mystery():
+#     return {'sanity': 'Hello'}
