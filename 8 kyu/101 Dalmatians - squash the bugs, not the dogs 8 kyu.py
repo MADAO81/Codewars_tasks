5afd5ea44d86e27fb4c28d33ffa447c8184d9a79
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-07-19 20:41:33 +0300

    Update 101 Dalmatians - squash the bugs, not the dogs 8 kyu.py

--- 8 kyu/101 Dalmatians - squash the bugs, not the dogs 8 kyu.py
+++ 8 kyu/101 Dalmatians - squash the bugs, not the dogs 8 kyu.py
@@ -5,12 +5,16 @@
 # The number of dogs will always be a number and there will always be at least 1 dog.
 
 def how_many_dalmatians(n):
-    dogs =  ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"]
-    if n <= 10:
-        return dogs[0] 
-    elif n <= 50:
-        return dogs[1] 
-    elif n == 101:
-        return dogs[3] 
-    else:
-        return dogs[2]
+    dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"]
+    return dogs[0] if n <= 10 else dogs[1] if n <=50 else dogs[3] if n == 101 else dogs[2]
+
+# def how_many_dalmatians(n):
+#     dogs =  ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"]
+#     if n <= 10:
+#         return dogs[0] 
+#     elif n <= 50:
+#         return dogs[1] 
+#     elif n == 101:
+#         return dogs[3] 
+#     else:
+#         return dogs[2]
