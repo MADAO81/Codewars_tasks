f01456f97b886ddf975b62eb06e7a79de34ecc40
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-07-27 11:54:53 +0300

    Create Factorial 7 kyu.py

---
+++ 7 kyu/Factorial 7 kyu.py
@@ -0,0 +1,14 @@
+# Your task is to write function factorial.
+
+# https://en.wikipedia.org/wiki/Factorial
+
+import math
+def factorial(n):
+    return math.factorial(n)
+
+
+# def factorial(n):
+#     j = 1
+#     for i in range(1, n+1):
+#        j *= i
+#     return j  
