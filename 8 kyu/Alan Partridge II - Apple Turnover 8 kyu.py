e359404ddd3a4d01ebc7931d690e6f25fbb87b1c
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-07-29 17:23:42 +0300

    Create Alan Partridge II - Apple Turnover 8 kyu.py

---
+++ 8 kyu/Alan Partridge II - Apple Turnover 8 kyu.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/580a094553bd9ec5d800007d/train/python
+
+def apple(x):
+    if int(x)**2 >1000:
+        return "It's hotter than the sun!!"
+    else:
+        return "Help yourself to a honeycomb Yorkie for the glovebox."
