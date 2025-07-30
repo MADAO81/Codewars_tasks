60550292ba8fcc58b6ed2af500e6497900343314
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-07-30 12:17:08 +0300

    Create Title Case 6 kyu.py

---
+++ 6 kyu/Title Case 6 kyu.py
@@ -0,0 +1,30 @@
+# Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+
+# Arguments (Haskell)
+# First argument: space-delimited list of minor words that must always be lowercase except for the first word in the string.
+# Second argument: the original string to be converted.
+# Arguments (Other languages)
+# First argument (required): the original string to be converted.
+# Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word in the string. The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+# Example
+# title_case('a clash of KINGS', 'a an the of') # should return: 'A Clash of Kings'
+# title_case('THE WIND IN THE WILLOWS', 'The In') # should return: 'The Wind in the Willows'
+# title_case('the quick brown fox') # should return: 'The Quick Brown Fox'
+
+def title_case(title, minor_words=''):
+    title = title.split(" ")
+    minor_words = minor_words.split(" ")
+    minor_words = list(map(lambda x:x.lower(), minor_words))
+    result = title[0].title()
+    for word in title[1:]:
+        if word.lower() not in minor_words:
+            result += " " +  word.title()
+        else:
+            result += " " + word.lower()
+    return result
+
+
+# def title_case(title, minor_words=''):
+#     title = title.capitalize().split()
+#     minor_words = minor_words.lower().split()
+#     return ' '.join([word if word in minor_words else word.capitalize() for word in title])
