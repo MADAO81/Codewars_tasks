c88575337fe7a15f117217c0be4372501071ae0c
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-08-01 16:39:24 +0300

    Create No oddities here 7 kyu.py

---
+++ 7 kyu/No oddities here 7 kyu.py
@@ -0,0 +1,13 @@
+# Write a small function that returns the values of an array that are not odd.
+# All values in the array will be integers. Return the good values in the order they are given.
+
+def no_odds(values):
+    result = []
+    for i in values:
+        if i%2 == 0:
+            result.append(i)
+    return result
+
+
+# def no_odds(values):
+#     return [i for i in values if i % 2 == 0]
