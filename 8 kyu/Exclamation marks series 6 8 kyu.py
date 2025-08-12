dae95b37be43cb9ecf95946c17cfd0cf1adf7240
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-08-12 21:07:38 +0300

    Create Exclamation marks series 6 8 kyu.py

---
+++ 8 kyu/Exclamation marks series 6 8 kyu.py
@@ -0,0 +1,27 @@
+# Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+# Examples
+# remove("Hi!",1) === "Hi"
+# remove("Hi!",100) === "Hi"
+# remove("Hi!!!",1) === "Hi!!"
+# remove("Hi!!!",100) === "Hi"
+# remove("!Hi",1) === "Hi"
+# remove("!Hi!",1) === "Hi!"
+# remove("!Hi!",100) === "Hi"
+# remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+# remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+# remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+# remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+
+def remove(st, n):
+    return st.replace("!","",n)
+
+# def remove(s, n):
+#     result=[]
+#     k=1
+#     for i in range(len(s)):
+#             if s[i]=="!" and k<=n:
+#                 k+=1
+#             else:
+#                 result.append(s[i])
+#     return "".join(result)
