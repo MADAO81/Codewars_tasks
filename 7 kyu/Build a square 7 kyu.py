c941f3b8d92b1058c7a854fd1ce84db7fddb6adf
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-08-21 09:40:33 +0300

    Create Build a square 7 kyu.py

---
+++ 7 kyu/Build a square 7 kyu.py
@@ -0,0 +1,17 @@
+# I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+
+# Example
+# n = 3, so I expect a 3x3 square back just like below as a string:
+
+# +++
+# +++
+# +++
+
+# def generate_shape(n):
+#     square = ""
+#     for i in range(n):
+#         square += str("+"*n)+"\n"
+#     return square[0:len(square)-1]
+
+def generateShape(integer):
+    return '\n'.join('+' * integer for i in range(integer))
