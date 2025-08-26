3ec00c375fca8bfc0a47ccee3389d0740f682800
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-08-26 17:18:07 +0300

    Create Simple remove duplicates 7 kyu.py

---
+++ 7 kyu/Simple remove duplicates 7 kyu.py
@@ -0,0 +1,28 @@
+# Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+
+# Example:
+# For input: [3, 4, 4, 3, 6, 3]
+
+# remove the 3 at index 0
+# remove the 4 at index 1
+# remove the 3 at index 3
+# Expected output: [4, 6, 3]
+
+# More examples can be found in the test cases.
+
+# Good luck!
+
+# def solve(arr): 
+#     unique = []
+#     for i in reversed(arr):
+#         if i not in unique:
+#             unique.append(i)
+#     return list(reversed(unique))
+
+
+def solve(arr): 
+    re = []
+    for i in arr[::-1]:
+        if i not in re:
+            re.append(i)
+    return re[::-1]
