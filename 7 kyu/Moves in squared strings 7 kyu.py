b2cafb9b0cf27261e0edc16f88dde6e540888999
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-08-28 09:01:12 +0300

    Create Moves in squared strings 7 kyu.py

---
+++ 7 kyu/Moves in squared strings 7 kyu.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/56dbe0e313c2f63be4000b25/solutions/python
+
+
+def vert_mirror(s):
+    return "\n".join(line[::-1] for line in s.split("\n"))
+
+def hor_mirror(s):
+    return "\n".join(s.split("\n")[::-1])
+
+def oper(fct, s):
+    return fct(s)
