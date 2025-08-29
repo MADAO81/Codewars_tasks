be4b2b360971b22335964ba069ca69cb9d2444a8
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-08-29 09:15:15 +0300

    Create Pick peaks 5 kyu.py

---
+++ 5 kyu/Pick peaks 5 kyu.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/5279f6fe5ab7f447890006a7/train/python
+
+def pick_peaks(arr):
+    pos = []
+    prob_peak = False
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i-1]:
+            prob_peak = i
+        elif arr[i] < arr[i-1] and prob_peak:
+            pos.append(prob_peak)
+            prob_peak = False
+    return {'pos':pos, 'peaks':[arr[i] for i in pos]}
