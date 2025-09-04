676d162fed7cf35c55c5a36c2e431cf4f24805ed
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-09-04 12:10:56 +0300

    Create  Find the Slope 8 kyu.py

---
+++ 8 kyu/Find the Slope 8 kyu.py
@@ -0,0 +1,23 @@
+# Given an array of 4 integers
+# [a,b,c,d] representing two points (a, b) and (c, d), return a string representation of the slope of the line joining these two points.
+
+# For an undefined slope (division by 0), return undefined . Note that the "undefined" is case-sensitive.
+
+#    a:x1
+#    b:y1
+#    c:x2
+#    d:y2
+# Assume that [a,b,c,d] and the answer are all integers (no floating numbers!). Slope: https://en.wikipedia.org/wiki/Slope
+
+
+# def find_slope(points):
+#     x1, y1, x2, y2 = points
+#     if x2 - x1 == 0:
+#         return "undefined"
+#     return str((y2 - y1) // (x2 - x1))
+
+def find_slope(points):
+    try:
+        return str((points[3] - points[1]) // (points[2] - points[0]))
+    except ZeroDivisionError:
+        return "undefined"
