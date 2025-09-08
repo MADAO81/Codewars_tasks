7d0f642de813c16f6de68809e54ca3d546077158
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-09-08 08:19:02 +0300

    Create  Be Concise IV - Index of an element in an array 8 kyu.py

---
+++ 8 kyu/Be Concise IV - Index of an element in an array 8 kyu.py
@@ -0,0 +1,12 @@
+# Provided is a function Kata which accepts two parameters in the following order: 
+# array, element and returns the index of the element if found and "Not found" otherwise. 
+# our task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. 
+# (no more than 161) You may assume that all array elements are unique.
+
+def find(a, e):
+        return a.index(e) if e in a else 'Not found'
+
+
+# def find(arr, e):
+#     try: return arr.index(e)
+#     except: return "Not found"
