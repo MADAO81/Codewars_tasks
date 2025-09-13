83fbd167c6dea9185959d7a1d28d196e551c6ff5
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-09-13 19:32:47 +0300

    Create Smallest value of an array 7 kyu.py

---
+++ 7 kyu/Smallest value of an array 7 kyu.py
@@ -0,0 +1,13 @@
+# Write a function that can return the smallest value of an array or the index of that value. 
+# The function's 2nd parameter will tell whether it should return the value or the index.
+# Assume the first parameter will always be an array filled with at least 1 number and no duplicates. 
+# Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+
+# Some examples:
+
+# ([1,2,3,4,5], "value") should return 1
+# ([1,2,3,4,5], "index") should return 0
+
+import numpy
+def find_smallest(numbers, to_return):
+    return numpy.min(numbers) if to_return == 'value' else numpy.argmin(numbers)
