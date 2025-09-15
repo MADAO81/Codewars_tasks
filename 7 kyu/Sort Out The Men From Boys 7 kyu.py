261925b7a6408746e7a959d3d3f705308516d482
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-09-15 10:00:26 +0300

    Update Sort Out The Men From Boys 7 kyu.py

--- 7 kyu/Sort Out The Men From Boys 7 kyu.py
+++ 7 kyu/Sort Out The Men From Boys 7 kyu.py
@@ -4,11 +4,22 @@
 #     men = []
 #     boys = []
 #     for i in set(arr):
-#         if i%2 != 0:
+#         if i%2 == 0:
 #             men.append(i)
 #         else:
 #             boys.append(i)
-#     return sorted(boys) + sorted(men,reverse = True)
+#     return sorted(men) + sorted(boys,reverse = True)
+
+# def men_from_boys(arr):
+#     men = []
+#     boys = []
+#     for i in sorted(set(arr)):
+#         if i % 2 == 0:
+#             men.append(i)
+#         else:
+#             boys.append(i)
+#     return men + boys[::-1]
+
 
 def men_from_boys(arr):
     return sorted([i for i in set(arr) if i % 2 == 0]) + sorted([i for i in set(arr) if i % 2 != 0], reverse=True)
