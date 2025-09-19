4f69f200641fe43b795d73c060828616dbe7c752
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-09-19 15:37:12 +0300

    Create Highest Rank Number in an Array 6 kyu.py

---
+++ 6 kyu/Highest Rank Number in an Array 6 kyu.py
@@ -0,0 +1,22 @@
+# Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
+
+# Note: no empty arrays will be given.
+
+# Examples
+# [12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+# [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+# [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+
+
+# from collections import Counter
+
+# def highest_rank(arr):
+#     if arr:
+#         c = Counter(arr)
+#         m = max(c.values())
+#         return max(k for k,v in c.items() if v==m)
+
+
+
+def highest_rank(arr):
+    return max(sorted(arr,reverse=True), key=arr.count)
