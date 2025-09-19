2badb5ff903e6393421f3e66614c37e3fffaf0e2
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-09-19 10:13:10 +0300

    Create Currying functions multiply all elements in an array 7 kyu.py

---
+++ 7 kyu/Currying functions multiply all elements in an array 7 kyu.py
@@ -0,0 +1,17 @@
+# To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. 
+# This function must return another function, which takes a single integer as an argument and returns a new array.
+
+# The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+# Example:
+
+# multiply_all([1, 2, 3])(2); // => [2, 4, 6]
+# You must not mutate the original array.
+
+# def multiply_all(arr):
+#     def m(n):
+#         return [i*n for i in arr]
+#     return m
+
+def multiply_all(integers: list):
+    return lambda n: [n * i for i in integers]
