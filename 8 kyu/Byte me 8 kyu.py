f275665c0f03da436d97fed85839e29a3fc250fe
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-09-25 12:55:58 +0300

    Create Byte me 8 kyu.py

---
+++ 8 kyu/Byte me 8 kyu.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/636f26f52aae8fcf3fa35819/train/python
+
+import sys
+
+# return the total byte size of the object. 
+def total_bytes(object):
+    return sys.getsizeof(object)
