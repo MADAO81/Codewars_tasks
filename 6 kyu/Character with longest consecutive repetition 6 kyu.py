55a445997dd0bf6f05de655b054e7e275dd591ae
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-10-03 16:40:26 +0300

    Create Character with longest consecutive repetition 6 kyu.py

---
+++ 6 kyu/Character with longest consecutive repetition 6 kyu.py
@@ -0,0 +1,31 @@
+# For a given string s find the character c (or C) with longest consecutive repetition and return:
+
+# (c, l)
+# where l (or L) is the length of the repetition. If there are two or more characters with the same l return the first in order of appearance.
+
+# For empty string return:
+
+# ('', 0)
+# Happy coding! :)
+
+
+# def longest_repetition(chars):
+#     max_char, max_count = '', 0
+#     char, count = '', 0
+#     for c in chars:
+#         if c != char:
+#             count, char = 0, c
+#         count += 1
+#         if count > max_count:
+#             max_char, max_count = char, count
+#     return max_char, max_count
+
+
+
+import re
+
+def longest_repetition(chars):
+    if not chars: return ("", 0)
+    
+    longest = max(re.findall(r"((.)\2*)", chars), key=lambda x: len(x[0]))
+    return (longest[1], len(longest[0]))
