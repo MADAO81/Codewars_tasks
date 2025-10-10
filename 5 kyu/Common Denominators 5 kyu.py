92d93764163f9e918dfdc54dbfbb36a583afd1e9
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-10-10 14:19:58 +0300

    Update Common Denominators 5 kyu.py

--- 5 kyu/Common Denominators 5 kyu.py
+++ 5 kyu/Common Denominators 5 kyu.py
@@ -20,6 +20,18 @@
 # def lcm(a, b):
 #     return a * b // gcd(a, b)
 
+
+
+# from fractions import gcd
+# def convertFracts(lst):
+#     D = 1
+#     for _,d in lst:
+#         D*= d//gcd(d,D)
+#     return [[D*n//d,D] for n,d in lst]
+
+
+
+
 import math
 import functools
 
