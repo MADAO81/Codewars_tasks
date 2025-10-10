6c8d4fc2777c7576b1c203684c111c1ac2e63ecb
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-10-10 09:12:24 +0300

    Create Debug Sum of Digits of a Number 7 kyu.py

---
+++ 7 kyu/Debug Sum of Digits of a Number 7 kyu.py
@@ -0,0 +1,16 @@
+# Debug   function getSumOfDigits that takes positive integer to calculate sum of its digits. Assume that argument is an integer.
+
+# Example
+# 123  => 6
+# 223  => 7
+# 1337 => 14
+
+# def get_sum_of_digits(num):
+#     return sum(map(int, str(num)))
+
+def get_sum_of_digits(num):
+    sum = 0
+    digits = str(num)
+    for x in digits:
+        sum += int(x)
+    return sum
