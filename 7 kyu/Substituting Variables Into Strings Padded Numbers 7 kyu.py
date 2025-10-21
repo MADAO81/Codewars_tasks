f83a09612a3ca5b8c1545c325db598548abc90df
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-10-21 09:18:25 +0300

    Create Substituting Variables Into Strings Padded Numbers 7 kyu.py

---
+++ 7 kyu/Substituting Variables Into Strings Padded Numbers 7 kyu.py
@@ -0,0 +1,14 @@
+# Substituting Variables Into Strings Padded Numbers
+# Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+# Example:
+
+# solution(5); // should return "Value is 00005"
+
+
+# def solution(value):
+#     return "Value is %05d" % value
+
+def solution(value):
+    value = str(value)
+    return f"Value is {value.zfill(5)}"
