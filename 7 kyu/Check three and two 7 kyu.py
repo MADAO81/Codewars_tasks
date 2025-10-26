57b743c91063a421712ea2fb2b173f9c2b822be1
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-10-26 17:04:53 +0300

    Update Check three and two 7 kyu.py

--- 7 kyu/Check three and two 7 kyu.py
+++ 7 kyu/Check three and two 7 kyu.py
@@ -1,11 +1,14 @@
 # https://www.codewars.com/kata/5a9e86705ee396d6be000091/train/python
 
 
-from collections import Counter
+# from collections import Counter
+
+# def check_three_and_two(array):
+#     counts = Counter(array).values()
+#     if all(x in counts for x in[3,2]):
+#         return True
+#     return False
+    
 
 def check_three_and_two(array):
-    counts = Counter(array).values()
-    if all(x in counts for x in[3,2]):
-        return True
-    return False
-    
+    return { array.count(x) for x in set(array) } == {2, 3}
