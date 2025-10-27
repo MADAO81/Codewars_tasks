8c61df382909872eef055d86c5f72e60193e9592
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-10-27 09:36:55 +0300

    Create Discover The Original Price 7 kyu.py

---
+++ 7 kyu/Discover The Original Price 7 kyu.py
@@ -0,0 +1,4 @@
+# https://www.codewars.com/kata/552564a82142d701f5001228/train/python
+
+def discover_original_price(discounted_price, sale_percentage):
+    return discounted_price/(1-sale_percentage/100)
