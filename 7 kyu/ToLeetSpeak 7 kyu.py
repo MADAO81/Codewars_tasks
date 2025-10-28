ec612109a1f3819b391253651412faabe083d323
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-10-28 09:52:26 +0300

    Update ToLeetSpeak 7 kyu.py

--- 7 kyu/ToLeetSpeak 7 kyu.py
+++ 7 kyu/ToLeetSpeak 7 kyu.py
@@ -1,43 +1,47 @@
 # https://www.codewars.com/kata/57c1ab3949324c321600013f/train/python
 
 
-def to_leet_speak(s):
-    new_string = [letter for letter in s]
-    leet_alphabet = {
-    "A" : '@',
-    "B" : '8',
-    "C" : '(',
-    "D" : 'D',
-    "E" : '3',
-    "F" : 'F',
-    "G" : '6',
-    "H" : '#',
-    "I" : '!',
-    "J" : 'J',
-    "K" : 'K',
-    "L" : '1',
-    "M" : 'M',
-    "N" : 'N',
-    "O" : '0',
-    "P" : 'P',
-    "Q" : 'Q',
-    "R" : 'R',
-    "S" : '$',
-    "T" : '7',
-    "U" : 'U',
-    "V" : 'V',
-    "W" : 'W',
-    "X" : 'X',
-    "Y" : 'Y',
-    "Z" : '2'
-     }
+# def to_leet_speak(s):
+#     new_string = [letter for letter in s]
+#     leet_alphabet = {
+#     "A" : '@',
+#     "B" : '8',
+#     "C" : '(',
+#     "D" : 'D',
+#     "E" : '3',
+#     "F" : 'F',
+#     "G" : '6',
+#     "H" : '#',
+#     "I" : '!',
+#     "J" : 'J',
+#     "K" : 'K',
+#     "L" : '1',
+#     "M" : 'M',
+#     "N" : 'N',
+#     "O" : '0',
+#     "P" : 'P',
+#     "Q" : 'Q',
+#     "R" : 'R',
+#     "S" : '$',
+#     "T" : '7',
+#     "U" : 'U',
+#     "V" : 'V',
+#     "W" : 'W',
+#     "X" : 'X',
+#     "Y" : 'Y',
+#     "Z" : '2'
+#      }
 
-    result = []
-    for i in new_string:
-        if i == " ":
-            result.append(i)
-        for k, v in leet_alphabet.items():
-            if i == k:
-                result.append(v)
-    return "".join(result)
-    return s
+#     result = []
+#     for i in new_string:
+#         if i == " ":
+#             result.append(i)
+#         for k, v in leet_alphabet.items():
+#             if i == k:
+#                 result.append(v)
+#     return "".join(result)
+#     return s
+
+
+def to_leet_speak(str):
+    return str.translate(str.maketrans("ABCEGHILOSTZ", "@8(36#!10$72"))
