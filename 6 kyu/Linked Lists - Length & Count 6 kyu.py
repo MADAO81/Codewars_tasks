e2ab67a8a1ca5a38baf4b70a5cc008ac6ab5ca8d
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-10-30 09:23:46 +0300

    Update Linked Lists - Length & Count 6 kyu.py

--- 6 kyu/Linked Lists - Length & Count 6 kyu.py
+++ 6 kyu/Linked Lists - Length & Count 6 kyu.py
@@ -1,17 +1,38 @@
 # https://www.codewars.com/kata/55beec7dd347078289000021/train/python
 
-class Node:
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+
+# def length(x):
+#     if x is None:
+#         return 0
+#     return length(x.next) + 1
+
+  
+# def count(x,y):
+#     if x is None:
+#         return 0
+#     return count(x.next, y) + (1 if x.data == y else 0)
+
+
+class Node(object):
     def __init__(self, data):
         self.data = data
         self.next = None
-
-def length(x):
-    if x is None:
-        return 0
-    return length(x.next) + 1
-
+    
+def length(node):
+    leng = 0
+    while node:
+        leng += 1
+        node = node.next
+    return leng
   
-def count(x,y):
-    if x is None:
-        return 0
-    return count(x.next, y) + (1 if x.data == y else 0)
+def count(node, data):
+    c = 0
+    while node:
+        if node.data==data:
+            c += 1
+        node = node.next
+    return c
