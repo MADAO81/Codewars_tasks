f3667bb4a5fa9e30cad221fe9c70f2e30e070255
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-11-05 09:12:14 +0300

    Create Head, Tail, Init and Last 7 kyu.py

---
+++ 7 kyu/Head, Tail, Init and Last 7 kyu.py
@@ -0,0 +1,20 @@
+# https://www.codewars.com/kata/54592a5052756d5c5d0009c3/train/python
+
+# head = lambda array: array[0]
+# tail = lambda array: array[1:]
+# init = lambda array: array[:-1]
+# last = lambda array: array[-1]
+
+
+
+def head(lst):
+    return lst[0]
+
+def tail(lst):
+    return lst[1:]
+
+def init(lst):
+    return lst[:-1]
+
+def last(lst):
+    return lst[-1]
