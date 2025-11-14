cc6e0162604e58f0a2052b1284152aec20244e1b
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-11-14 09:31:45 +0300

    Create Formatting decimal places 1 7 kyu.py

---
+++ 7 kyu/Formatting decimal places 1 7 kyu.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/5641c3f809bf31f008000042/train/python
+
+# import math
+# def two_decimal_places(number):
+#     return math.trunc(100*number)/100
+
+def two_decimal_places(number):
+    return int (number * 100) / 100.0
