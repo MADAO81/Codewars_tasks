6901c5c3dcadac04c89fd10e49b2f31c814ff2a0
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-11-19 10:24:50 +0300

    Create Reverse_the_bits_in_an_integer_7_kyu.py

---
+++ 7 kyu/Reverse_the_bits_in_an_integer_7_kyu.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/5959ec605595565f5c00002b/train/python
+
+# def reverse_bits(n):
+#     return int(str(bin(n).replace('0b',''))[::-1],2)
+
+def reverse_bits(n):
+    return int(bin(n)[:1:-1],2)
