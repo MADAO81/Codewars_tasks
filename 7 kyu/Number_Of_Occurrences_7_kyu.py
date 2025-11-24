c9ecde19e1cd2655035429f313490eeae01922cc
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-11-24 17:18:27 +0300

    Create Number_Of_Occurrences_7_kyu.py

---
+++ 7 kyu/Number_Of_Occurrences_7_kyu.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/52829c5fe08baf7edc00122b/train/python
+
+def number_of_occurrences(element, sample):
+
+#     count = 0
+#     for el in sample:
+#         if el == element:
+#             count +=1
+#     return count
+
+    return sample.count(element)
