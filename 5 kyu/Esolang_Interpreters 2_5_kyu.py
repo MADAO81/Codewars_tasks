c31a21411f7edd4365f32ed3b375d3bfab3a8605
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-11-27 12:13:10 +0300

    Create Esolang_Interpreters 2_5_kyu.py

---
+++ 5 kyu/Esolang_Interpreters 2_5_kyu.py
@@ -0,0 +1,22 @@
+# https://www.codewars.com/kata/58678d29dbca9a68d80000d7/train/python
+
+def interpreter(code, tape):
+    tape = list(map(int, tape))
+    ptr = step = loop = 0
+    
+    while 0 <= ptr < len(tape) and step < len(code):
+        command = code[step]
+        
+        if loop:
+            if   command == "[": loop += 1
+            elif command == "]": loop -= 1
+        
+        elif command == ">": ptr += 1
+        elif command == "<": ptr -= 1
+        elif command == "*": tape[ptr] ^= 1        
+        elif command == "[" and tape[ptr] == 0: loop += 1
+        elif command == "]" and tape[ptr] == 1: loop -= 1
+    
+        step += 1 if not loop else loop // abs(loop)
+    
+    return "".join(map(str, tape))
