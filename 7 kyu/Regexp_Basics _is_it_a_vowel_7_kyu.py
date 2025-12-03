48ddca6af3ca56b06843189ebdf000fbda2c20e4
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-12-03 09:03:01 +0300

    Create Regexp_Basics _is_it_a_vowel_7_kyu.py

---
+++ 7 kyu/Regexp_Basics _is_it_a_vowel_7_kyu.py
@@ -0,0 +1,15 @@
+# Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u, uppercase or lowercase), and false otherwise.
+
+# def is_vowel(s): 
+#     vowels = 'aeiou'
+#     return len(s)==1 and s.lower() in vowels
+
+
+# def is_vowel(s):
+#     return s.lower() in set("aeiou")
+
+
+import re
+def is_vowel(s): 
+    regex = r'^[aeiou]{1}$'
+    return bool(re.match(regex, re.escape(s), re.IGNORECASE))
