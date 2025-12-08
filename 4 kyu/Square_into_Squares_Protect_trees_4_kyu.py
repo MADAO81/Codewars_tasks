625a5dd17398b128aec36925d1f4c7c9d149d9dd
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-12-08 10:15:39 +0300

    Update Square_into_Squares_Protect_trees_4_kyu.py

--- 4 kyu/Square_into_Squares_Protect_trees_4_kyu.py
+++ 4 kyu/Square_into_Squares_Protect_trees_4_kyu.py
@@ -1,16 +1,9 @@
 # https://www.codewars.com/kata/54eb33e5bc1a25440d000891/train/python
 
-def decompose(n):
-    goal = 0
-    result = [n]
-    while result:
-        current = result.pop()
-        goal += current ** 2
-        for i in range(current -1,0,-1):
-            if goal - (i**2) >= 0:
-                goal -= i**2
-                result.append(i)
-                if goal == 0:
-                    result.sort()
-                    return result
-    return None
+
+def decompose(n, a=None):
+    if a == None: a = n*n
+    if a == 0: return []
+    for m in range(min(n-1, int(a ** .5)), 0, -1):
+        sub = decompose(m, a - m*m)
+        if sub != None: return sub + [m]
