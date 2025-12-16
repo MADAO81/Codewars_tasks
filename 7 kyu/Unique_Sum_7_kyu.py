b9ffa53dfb1b700d38f8b31df6235e243cd7994e
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-12-16 09:25:02 +0300

    Create Unique_Sum_7_kyu.py

---
+++ 7 kyu/Unique_Sum_7_kyu.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/56b1eb19247c01493a000065/train/python
+
+# def unique_sum(lst):
+#     if lst:
+#         return sum(set(lst))
+
+
+def unique_sum(lst):
+    return sum(set(lst)) if lst else None
