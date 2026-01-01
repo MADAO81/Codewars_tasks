3792795fbd6ed7884224e613c9fe3ad5c2ede7cb
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-01-01 14:03:02 +0300

    Create Dont_rely_on_luck_6_kyu.py

---
+++ 6 kyu/Dont_rely_on_luck_6_kyu.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/5268af3872b786f006000228/train/python
+
+
+# from random import randint
+
+# class CheatingNumber:
+#     def __eq__(self, x):
+#         return True
+      
+# guess = CheatingNumber()
+
+
+from random import randint,seed
+seed(1)
+guess = randint(1,100)
+seed(1)
