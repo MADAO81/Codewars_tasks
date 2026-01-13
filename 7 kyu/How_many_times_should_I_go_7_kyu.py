9b673aec19b85bc9ae69e1f93a0a3a4b9913e2cf
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-01-13 17:26:59 +0300

    Create How_many_times_should_I_go_7_kyu.py

---
+++ 7 kyu/How_many_times_should_I_go_7_kyu.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/57efcb78e77282f4790003d8/train/python
+
+# import math 
+# def how_many_times(annual_price, individual_price):
+#     return math.ceil(annual_price / individual_price)
+
+
+from math import ceil
+
+def how_many_times(annual_price, individual_price):
+    return ceil(annual_price / individual_price)
