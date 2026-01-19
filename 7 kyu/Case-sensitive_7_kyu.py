64962e21b0758c0f1806b6c818cd0759a5cafe8c
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-01-19 09:20:08 +0300

    Update and rename Case-sensitive 7 kyu.py to Case-sensitive_7_kyu.py

--- 7 kyu/Case-sensitive_7_kyu.py
+++ 7 kyu/Case-sensitive_7_kyu.py
@@ -4,16 +4,16 @@
 
 # For example, case_sensitive('codewars') returns [True, []], but case_sensitive('codeWaRs') returns [False, ['W', 'R']].
 
-def case_sensitive(s):
-    result = []
-    for ch in list(s):
-        if ch == ch.upper():
-            result.append(ch)
-    if result == []:
-        return [True, result]
-    else:
-        return [False, result]
-
-
 # def case_sensitive(s):
-#     return [s.islower() or not s, [c for c in s if c.isupper()]]
+#     result = []
+#     for ch in list(s):
+#         if ch == ch.upper():
+#             result.append(ch)
+#     if result == []:
+#         return [True, result]
+#     else:
+#         return [False, result]
+
+
+def case_sensitive(s):
+    return [s.islower() or not s, [c for c in s if c.isupper()]]
