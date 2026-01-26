379b7389c261497ce05d074e4c6cdbb1677e6745
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-01-26 09:03:04 +0300

    Update and rename Credit Card Checker 7 kyu.py to Credit_Card_Checker_7_kyu.py

--- 7 kyu/Credit_Card_Checker_7_kyu.py
+++ 7 kyu/Credit_Card_Checker_7_kyu.py
@@ -18,18 +18,18 @@
 
 # "5457 6238 9323 4311" --> False
 
-def valid_card(card):
-    control_sum = 0
-    for i, a in enumerate(reversed(card.replace(' ', ''))):
-        if i % 2:
-            value = int(a) * 2
-            control_sum += value - 9 if value > 8 else value
-        else:
-            control_sum += int(a)
-    return control_sum % 10 == 0
-    
-    
 # def valid_card(card):
-#     s = list(map(int, str(card.replace(' ', ''))))
-#     s[0::2] = [d * 2 - 9 if d * 2 > 9 else d * 2 for d in s[0::2]]
-#     return sum(s) % 10 == 0
+#     control_sum = 0
+#     for i, a in enumerate(reversed(card.replace(' ', ''))):
+#         if i % 2:
+#             value = int(a) * 2
+#             control_sum += value - 9 if value > 8 else value
+#         else:
+#             control_sum += int(a)
+#     return control_sum % 10 == 0
+    
+    
+def valid_card(card):
+    s = list(map(int, str(card.replace(' ', ''))))
+    s[0::2] = [d * 2 - 9 if d * 2 > 9 else d * 2 for d in s[0::2]]
+    return sum(s) % 10 == 0
