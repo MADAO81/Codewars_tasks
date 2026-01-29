85cf29c9a80369d59cfff49b40408f744e95a817
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-01-29 08:46:29 +0300

    Update and rename Bit Counting 6 kyu.py to Bit_Counting_6_kyu.py

--- 6 kyu/Bit_Counting_6_kyu.py
+++ 6 kyu/Bit_Counting_6_kyu.py
@@ -5,13 +5,15 @@
 
 # Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 
-def count_bits(n):
-    count = []
-    for ch in bin(n):
-        if ch == "1":
-            count.append(ch)
-    result = [int(item) for item in count]
-    return sum(result)
-    
-# def countBits(n):
-#     return bin(n).count("1")
+
+# def count_bits(n):
+#     count = []
+#     for ch in bin(n):
+#         if ch == "1":
+#             count.append(ch)
+#     result = [int(item) for item in count]
+#     return sum(result)
+
+
+def countBits(n):
+    return bin(n).count("1")
