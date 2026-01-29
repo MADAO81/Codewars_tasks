88d938d110b74807d6cbd0d8712fd341dce6a277
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-01-29 08:48:05 +0300

    Update and rename Bouncing Balls 6 kyu.py to Bouncing_Balls_6_kyu.py

--- 6 kyu/Bouncing_Balls_6_kyu.py
+++ 6 kyu/Bouncing_Balls_6_kyu.py
@@ -22,23 +22,24 @@
 
 # (Condition 2) not fulfilled).
 
-def bouncing_ball(h, bounce, window):
-    if h != 0 and 1>bounce>0 and window<h:
-        count = 1
-        current = h * bounce
-        while current > window:
-            current *= bounce
-            count += 2
-        return count
-    else:
-        return -1
+
+# def bouncing_ball(h, bounce, window):
+#     if h != 0 and 1>bounce>0 and window<h:
+#         count = 1
+#         current = h * bounce
+#         while current > window:
+#             current *= bounce
+#             count += 2
+#         return count
+#     else:
+#         return -1
 
 
-# def bouncingBall(h, bounce, window):
-#     if not 0 < bounce < 1: return -1
-#     count = 0
-#     while h > window:
-#         count += 1
-#         h *= bounce
-#         if h > window: count += 1
-#     return count or -1
+def bouncingBall(h, bounce, window):
+    if not 0 < bounce < 1: return -1
+    count = 0
+    while h > window:
+        count += 1
+        h *= bounce
+        if h > window: count += 1
+    return count or -1
