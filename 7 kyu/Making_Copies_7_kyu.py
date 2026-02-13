f5400223764c87b205cc909da4bf1d9afae1ca2f
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-02-13 09:15:54 +0300

    Create Making_Copies_7_kyu.py

---
+++ 7 kyu/Making_Copies_7_kyu.py
@@ -0,0 +1,19 @@
+# https://www.codewars.com/kata/53d2697b7152a5e13d000b82/train/python
+
+
+# def copy_list(l): 
+#     copy_l = []
+#     if l:
+#         for i in l:
+#             copy_l.append(i)
+#         return copy_l
+#     else:
+#         return []
+
+
+# def copy_list(l):
+#   return l.copy()
+
+
+def copy_list(l):
+    return l[:]
