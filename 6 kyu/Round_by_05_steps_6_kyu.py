352a7fabe0925b35e5db9756e45baaf74770349b
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-02-26 09:08:04 +0300

    Create Round_by_05_steps_6_kyu.py

---
+++ 6 kyu/Round_by_05_steps_6_kyu.py
@@ -0,0 +1,29 @@
+# https://www.codewars.com/kata/51f1342c76b586046800002a/train/python
+
+# import math
+
+# def solution(n):
+#     flr = math.floor(n)
+#     ceil = math.ceil(n)
+#     if n - flr < 0.25:
+#         return flr
+#     elif n - (flr + 0.5) >= 0.25:
+#         return ceil
+#     return flr+0.5
+
+
+
+# def solution(n):
+#     r = n % 1
+#     if 0 <= r < .25:
+#         z = 0
+#     elif .25 <= r < .75:
+#         z = .5
+#     else:
+#         z = 1
+#     return int(n) + z
+
+
+
+def solution(n):
+    return round(n * 2) / 2 if n != 4.25 else 4.5
