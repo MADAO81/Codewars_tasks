fcd2f8c48c8444fcea901841d87c689e5d63b35d
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-02-27 09:19:57 +0300

    Update and rename Jaden Casing Strings 7 kyu.py to Jaden_Casing_Strings_7_kyu.py

--- 7 kyu/Jaden_Casing_Strings_7_kyu.py
+++ 7 kyu/Jaden_Casing_Strings_7_kyu.py
@@ -10,9 +10,12 @@
 # Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 # Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
-def to_jaden_case(string):
-    return " ".join(word.capitalize() for word in string.split())
+
     
     
 # import string
 # toJadenCase = string.capwords
+
+
+def to_jaden_case(string):
+    return " ".join(word.capitalize() for word in string.split())
