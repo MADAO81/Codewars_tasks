b8b6378d247b7315ccbb8444a5755eff5c36f5f4
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-02-27 09:21:07 +0300

    Update and rename Keypad horror 7 kyu.py to Keypad_horror_7_kyu.py

--- 7 kyu/Keypad_horror_7_kyu.py
+++ 7 kyu/Keypad_horror_7_kyu.py
@@ -14,12 +14,13 @@
 # You get a string with numbers only
 
 
-import string
-def computer_to_phone(numbers):
-    keyb_numpad = "789456123"
-    cell_numpad =  "123456789"
-    conv = numbers.translate(str.maketrans(keyb_numpad,cell_numpad))
-    return conv
-
+# import string
 # def computer_to_phone(numbers):
-#     return numbers.translate(str.maketrans('123789', '789123'))
+#     keyb_numpad = "789456123"
+#     cell_numpad =  "123456789"
+#     conv = numbers.translate(str.maketrans(keyb_numpad,cell_numpad))
+#     return conv
+
+
+def computer_to_phone(numbers):
+    return numbers.translate(str.maketrans('123789', '789123'))
